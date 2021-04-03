3c75310a09f1b65cf8af8355cdefc7aee087ff74
Arthy <Arthy@ARTHY>
2021-04-04 00:16:43 +0530

    COMPLAINT AND CUSTOMER UTIL UPDATED

--- OnlineCourierManagementApplication/src/main/java/com/capg/ocma/util/ComplaintUtil.java
+++ OnlineCourierManagementApplication/src/main/java/com/capg/ocma/util/ComplaintUtil.java
@@ -1,5 +1,49 @@
 package com.capg.ocma.util;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.capg.ocma.entities.Complaint;
+import com.capg.ocma.model.ComplaintDTO;
+
+
+
 public class ComplaintUtil {
+	
+			public static List<ComplaintDTO> convertToComplaintDtoList(List<Complaint> list){
+				List<ComplaintDTO> dtolist = new ArrayList<ComplaintDTO>();
+				for(Complaint complaint : list) 
+					dtolist.add(convertToComplaintDTO(complaint));
+				return dtolist;
+			
+		}
+			
+			private static ComplaintDTO convertToComplaintDTO(Complaint complaint) {
+				ComplaintDTO complaintdto =new ComplaintDTO();
+				
+				complaintdto.setComplaintid(complaint.getComplaintid());
+				complaintdto.setConsignmentno(complaint.getConsignmentno());
+				complaintdto.setShortdescription(complaint.getShortdescription());
+				complaintdto.setDetaildescription(complaint.getDetaildescription());
+				
+				
+				return complaintdto;
+			}
+			/*public static Complaint convertToComplaint(ComplaintDTO dto) {
+				Complaint complaint =new Complaint();
+				
+				complaint.setComplaintid(dto.getComplaintid());
+				complaint.setConsignmentno(dto.getConsignmentno());
+				complaint.setShortdescription(dto.getShortdescription());
+				complaint.setDetaildescription(dto.getDetaildescription());
+				
+
+				return complaint;
+				
+			}*/
 
 }
+
+
--- OnlineCourierManagementApplication/src/main/java/com/capg/ocma/util/CustomerUtil.java
+++ OnlineCourierManagementApplication/src/main/java/com/capg/ocma/util/CustomerUtil.java
@@ -1,5 +1,48 @@
 package com.capg.ocma.util;
 
-public class CustomerUtil {
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.capg.ocma.entities.Customer;
+import com.capg.ocma.model.CustomerDTO;
+
+
+public class CustomerUtil {
+	public static List<CustomerDTO> convertToCustomerDtoList(List<Customer> list){
+		List<CustomerDTO> dtolist = new ArrayList<CustomerDTO>();
+		for(Customer customer : list) 
+			dtolist.add( convertToCustomerDTO(customer));
+		return dtolist;
+	
 }
+	
+	
+/*
+ * public static Customer convertToCustomer(CustomerDTO dto) { Customer customer
+ * =new Customer();
+ * 
+ * customer.setCustomerid(dto.getCustomerid());
+ * customer.setAadharno(dto.getAadharno());
+ * customer.setFirstname(dto.getFirstname());
+ * customer.setLastname(dto.getLastname());
+ * customer.setMobileno(dto.getMobileno());
+ * 
+ * return customer;
+ * 
+ * }
+ */
+	private static CustomerDTO convertToCustomerDTO(Customer customer) {
+		CustomerDTO dto =new CustomerDTO();
+		
+		dto.setCustomerid(customer.getCustomerid());
+		dto.setAadharno(customer.getAadharno());
+		dto.setFirstname(customer.getFirstname());
+		dto.setLastname(customer.getLastname());
+		dto.setMobileno(customer.getMobileno());
+		
+		return dto;
+	}
+}
+
+
