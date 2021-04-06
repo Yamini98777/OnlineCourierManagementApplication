d5691e70ad6f88cff238c43bb0f413b15aa15ad0
SRINIVAS MADIVAL <SRINIVAS MADIVAL@LAPTOP-HC65PU6G>
2021-04-06 22:18:31 +0530

    controller and implementations are

--- OnlineCourierManagementApplication/src/main/java/com/capg/ocma/controller/CourierController.java
+++ OnlineCourierManagementApplication/src/main/java/com/capg/ocma/controller/CourierController.java
@@ -1,11 +1,81 @@
 package com.capg.ocma.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capg.ocma.entities.Courier;
+import com.capg.ocma.exception.CourierNotFoundException;
+import com.capg.ocma.service.IShipmentService;
+
 @RestController
-@RequestMapping("/api/courier")
+@RequestMapping("/api/ocma/courier")
 public class CourierController 
 {
+   
+	@Autowired
+	IShipmentService shipmentservice;
+	
+	@PatchMapping("/initiate")
+	
+	public ResponseEntity<String> intiateShipmentTransaction(@RequestBody Courier courier) throws CourierNotFoundException{
+		
+		boolean flag= shipmentservice.initiateShipmentTransaction(courier);
+		if(flag) {
+			
+			return new ResponseEntity <> ("You have successfully initiated the shipment process for the courier with id " + courier, HttpStatus.OK);
+			
+		} else {
+			
+			return new ResponseEntity <> ("courier notFound", HttpStatus.NOT_FOUND);
+			
+		}	
+		
+	}
+	
+	@GetMapping("/checkStatus")
+	public ResponseEntity <String> checkShipmentStatusAction(@RequestBody Courier courier) throws CourierNotFoundException {
+		
+			String status = shipmentservice.checkShipmentStatus(courier);
+			return new ResponseEntity <> ("The status of the courier with courier id is: " + status, HttpStatus.OK);
+		
+	}
+	
+	@PatchMapping("/close")
+	public ResponseEntity <String> closeShipmentTransaction(@RequestBody Courier courier) throws CourierNotFoundException
+	 {
+		
+			boolean flag = shipmentservice.closeShipmentTransaction(courier);
+			if(flag) {
+				
+				return new ResponseEntity <> ("You have successfully closed the shipment process for the courier with id " + courier, HttpStatus.OK);
+				
+			} else {
+				
+				return new ResponseEntity <> ("courier not Found", HttpStatus.NOT_FOUND);	
+			}
+	}
 
+	@PatchMapping("/reject")
+	public ResponseEntity <String> rejectShipmentTransaction(@RequestBody Courier courier) throws CourierNotFoundException {
+		
+			boolean flag = shipmentservice.rejectShipmentTransaction(courier);
+			if(flag) {
+				
+				return new ResponseEntity <> ("You have successfully rejected the shipment process for the courier with id ", HttpStatus.OK);
+				
+			} else {
+				
+				return new ResponseEntity <> ("courier not Found", HttpStatus.NOT_FOUND);
+			
+			}
+		
+	}
+	
+	
 }
--- OnlineCourierManagementApplication/src/main/java/com/capg/ocma/service/IShipmentService.java
+++ OnlineCourierManagementApplication/src/main/java/com/capg/ocma/service/IShipmentService.java
@@ -2,12 +2,13 @@ package com.capg.ocma.service;
 
 import com.capg.ocma.entities.Courier;
 import com.capg.ocma.exception.CourierNotFoundException;
+import com.capg.ocma.model.CourierDTO;
 
 public interface IShipmentService {
 
-	public void initiateShipmentTransaction(Courier courier) throws CourierNotFoundException;
-	public void checkShipmentStatus(Courier courier) throws CourierNotFoundException;
-	public void closeShipmentTransaction(Courier courier) throws CourierNotFoundException;
-	public void rejectShipmentTransaction(Courier courier) throws CourierNotFoundException;
+	public boolean initiateShipmentTransaction(Courier courier) throws CourierNotFoundException;
+	public String checkShipmentStatus(Courier courier) throws CourierNotFoundException;
+	public boolean closeShipmentTransaction(Courier courier) throws CourierNotFoundException;
+	public boolean rejectShipmentTransaction(Courier courier) throws CourierNotFoundException;
 	
 }
