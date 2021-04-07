6ea89c0cfe2e7b4a2338b1bfd4613e2dffdb003f
Arthy <Arthy@ARTHY>
2021-04-07 22:45:39 +0530

    updated for get and just removed the unused import from all bramches thats y 11 imports

--- OnlineCourierManagementApplication/src/main/java/com/capg/ocma/repository/IComplaintDao.java
+++ OnlineCourierManagementApplication/src/main/java/com/capg/ocma/repository/IComplaintDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.capg.ocma.entities.Complaint;
-import com.capg.ocma.entities.Courier;
+
 
 @Repository
 public interface IComplaintDao extends JpaRepository<Complaint, Integer> {
--- OnlineCourierManagementApplication/src/main/java/com/capg/ocma/repository/UserLoginDao.java
+++ OnlineCourierManagementApplication/src/main/java/com/capg/ocma/repository/UserLoginDao.java
@@ -1,7 +1,6 @@
 package com.capg.ocma.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.capg.ocma.entities.UserLogin;
